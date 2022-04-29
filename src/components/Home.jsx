247a65b4914b4a10a633b7cb74ed03a5012551cf
kevmccall <kev_mccall@yahoo.com>
2022-04-28 19:48:11 -0600

    finished home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,16 +11,19 @@ const Home = () => {
     <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Kevin McCall </h1>
     <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'> I'm a Front-End Developer. </h2>
     <p className='text-[#8892b0] py-4 max-w-[700px]'>
-
+    I'm located in Denver, CO and I enjoy creating beautiful, intuitive web apps
+    to give users a rich experience. Web Development and Tech has been my passion
+    since I was a kid and I love seeing my designs come to life, while helping others.
     </p>
 
         <div>
         <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
         View Work
         <span className='group-hover:rotate-90 duration-300'>
-            <HiArrowNarrowRight className='ml-3 ' />
+        <HiArrowNarrowRight className='ml-3' />
         </span>
         </button>
+
     
     </div>
     </div>
