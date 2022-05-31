bf0c72fea1eaf60fe422a6149159c8e7214d56fc
kevmccall <kev_mccall@yahoo.com>
2022-05-31 15:32:51 -0600

    updated home/about created skills page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,7 @@ const About = () => {
           <div></div>
           
         </div>
+        
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           
           <div className="sm:text-right text-4xl font-bold">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,7 @@ const Home = () => {
     <p className='text-red-600'>Hi, my name is</p>
     <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Kevin McCall </h1>
     <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'> I'm a Front-End Developer. </h2>
+    
     <p className='text-[#8892b0] py-4 max-w-[700px]'>
     I'm located in Denver, CO and I enjoy creating beautiful, intuitive web apps
     to give users a rich experience. Web Development and Tech has been my passion
@@ -17,6 +18,7 @@ const Home = () => {
     </p>
 
         <div>
+          
         <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
         View Work
         <span className='group-hover:rotate-90 duration-300'>
