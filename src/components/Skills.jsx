913e87daff2253afcdeb6f45cb0b476a6ca874c6
kevmccall <kev_mccall@yahoo.com>
2022-05-31 15:38:49 -0600

    imported images to skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import GitHub from '../assets/github.png';
+
+
+
 const Skills = () => {
   return (
     <div>Skills</div>
