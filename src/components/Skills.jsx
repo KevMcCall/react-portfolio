d12c63cbdf11c26e74e3b67880d3c2cb7be2a544
kevmccall <kev_mccall@yahoo.com>
2022-06-09 13:31:12 -0600

    added skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,7 @@ import GitHub from '../assets/github.png';
 const Skills = () => {
   return (
 
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-screen bg-[#000000] text-gray-300'>
 
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -25,7 +25,7 @@ const Skills = () => {
 
           {/* Skills Icons */}
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+              <div className='shadow-md shadow-[#f0f1f3] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                   <p className='my-4'>HTML</p>
               </div>
