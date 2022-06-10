7b8ec110b92688579c631af595b5151efc774528
kevmccall <kev_mccall@yahoo.com>
2022-06-09 19:38:39 -0600

    added noopener noreferrer to links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,11 +73,11 @@ const Navbar = () => {
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0 '>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'> 
-          <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/kevin-mccall-/"> Linkedin <FaLinkedin size={30} /> </a></li>
+          <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/kevin-mccall-/" target="_blank" rel="noopener noreferrer"> Linkedin <FaLinkedin size={30} /> </a></li>
 
 
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'> 
-          <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/KevMcCall"> Github <FaGithub size={30} /> </a></li>
+          <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/KevMcCall" target="_blank" rel="noopener noreferrer"> Github <FaGithub size={30} /> </a></li>
         
 
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'> 
