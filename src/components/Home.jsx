ce8fdb2dda7ace6ee1f21ecc087a735e8d61e052
kevmccall <kev_mccall@yahoo.com>
2022-06-09 20:12:07 -0600

    added scroll to home button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
 
 const Home = () => {
   return (
@@ -20,10 +21,14 @@ const Home = () => {
         <div>
           
         <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
-        View Work
+        <Link to='work' smooth={true} duration={500}>
+            View Work
+        </Link>
         <span className='group-hover:rotate-90 duration-300'>
         <HiArrowNarrowRight className='ml-3' />
+        
         </span>
+        
         </button>
 
     
