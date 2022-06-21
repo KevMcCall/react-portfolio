2bb92314184ad58e5b5ef6ca26674983edf5678c
kevmccall <kev_mccall@yahoo.com>
2022-06-20 19:12:20 -0600

    added typewriter effect to header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
-import {Link} from 'react-scroll'
+import React from 'react';
+import {HiArrowNarrowRight} from 'react-icons/hi';
+import {Link} from 'react-scroll';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
@@ -10,7 +11,19 @@ const Home = () => {
     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
     <p className='text-red-600'>Hi, my name is</p>
     <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Kevin McCall </h1>
-    <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'> I'm a Front-End Developer. </h2>
+    <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'> 
+    <Typewriter 
+                 onInit={(typewriter) => {
+                    typewriter
+                    .typeString("I'm a Front-End Developer")
+                    .start();
+                 }}
+               />
+    </h2>
+
+
+
+    
     
     <p className='text-[#8892b0] py-4 max-w-[700px]'>
     I'm located in Denver, CO and I enjoy creating beautiful, intuitive web apps
